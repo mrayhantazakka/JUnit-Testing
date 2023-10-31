fa007492249d0d9f28776ca17373dad3d4f59150
mrayhantazakka <rayhantazakka1123@gmail.com>
2023-10-31 10:21:41 +0700

    penambahan comment

--- app/src/main/java/com/example/junit_test/Calculator.kt
+++ app/src/main/java/com/example/junit_test/Calculator.kt
@@ -1,8 +1,11 @@
 package com.example.junit_test
 
+//membuat class Calculator yang akan diuji
 class Calculator {
-
+    // membuat function didalam class calculator dengan parameter first sebagai angkla pertama,
+    // dan second sebagai angka kedua(angka yang akan ditambahkan) sesuai operator yang diinginkan
     fun add(first: Int, second: Int): Int{
+        // function add berisi mengembalikan nilai first dan second setelah ditambahkan
         return second + first
     }
 }
--- app/src/test/java/com/example/junit_test/CalculatorTest.kt
+++ app/src/test/java/com/example/junit_test/CalculatorTest.kt
@@ -3,13 +3,18 @@ package com.example.junit_test
 import org.junit.Test
 import kotlin.test.assertEquals
 
+//membuat class untuk testing pada class calculator
 class CalculatorTest {
-
+//membuat variabel calculator berdasarkan class Calculator
     val calculator = Calculator()
 
-    @Test
+    @Test //digunakan sebagai mendefinisikan bahwa ini class testing
+    //function sebagai testing
     fun testAddSuccess(){
+        // variabel result yang merupakan hasil penambahan angka first dan second
         val result = calculator.add(7, 8)
+        //mengecek result(hasil) apakah berhasil, jika hasilnya 15 maka testnya berhasil
         assertEquals(15, result)
+        //jika angka 15 diganti 14 harusnya eror, karena 7+8 harusnya hasilnya 15
     }
 }
